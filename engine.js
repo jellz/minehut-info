910be8d8c5b2d21d2fb1843e26f033903126f31e
Jellz <xdjellz@gmail.com>
2018-01-11 21:00:25 +1100

    now its just up to luke

--- engine.js
+++ engine.js
@@ -4,7 +4,16 @@ module.exports = function (filePath, options, callback) {
     fs.readFile(filePath, function (err, content) {
       if (err) return callback(err);
       var rendered = content.toString()
-      .replace('{{{msg}}}', options.msg);
+      .replace('{{{name}}}', options.name)
+      .replace('{{{name}}}', options.name)
+      .replace('{{{name}}}', options.name)
+      .replace('{{{ip}}}', options.ip)
+      .replace('{{{motd}}}', options.motd)
+      .replace('{{{onlinePlayers}}}', options.onlinePlayers)
+      .replace('{{{maxPlayers}}}', options.maxPlayers)
+      .replace('{{{creation}}}', options.creation)
+      .replace('{{{creditsPerDay}}}', options.creditsPerDay)
+      .replace('{{{status}}}', options.status);
       return callback(null, rendered);
     });
 };
